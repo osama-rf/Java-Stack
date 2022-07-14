0abb7b7fd0a3d11c9b58d6805848c93a98fc0074
osama-rf <osama.refae12901@icloud.com>
2022-07-14 21:19:42 +0300

    First

---
+++ Java Funds/First_Java/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("My Name is OSAMA" + '\n' + "I am 24 years old" + '\n' + "my hometown is jazan");
+    }
+}
