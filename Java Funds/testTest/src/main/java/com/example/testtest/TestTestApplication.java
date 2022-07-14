f9524addf9e2f2ae1e760289e7e9a802c151429b
osama-rf <osama.refae12901@icloud.com>
2022-07-14 21:19:42 +0300

    initial commit

--- Java Funds/testTest/src/main/java/com/example/testtest/TestTestApplication.java
+++ Java Funds/testTest/src/main/java/com/example/testtest/TestTestApplication.java
@@ -4,23 +4,24 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 
 public class TestTestApplication {
-    @RequestMapping("/hello")
+//    @RequestMapping("/hello")
     public static void main(String[] args) {
         SpringApplication.run(TestTestApplication.class, args);
     }
-    @RequestMapping("")
-    public String hello(){
-        return "Hello World";
+    @RequestMapping("/")
+    public String index(@RequestParam(value="q", required = false) String searchQuery) {
+        return "You searched for: " + searchQuery;
     }
-
-    @RequestMapping("/world")
-    public String world(){
-        return "Class level annotations are cool too";
-    }
+//
+//    @RequestMapping("/world")
+//    public String world(){
+//        return "Class level annotations are cool too";
+//    }
 
 }
---
+++ Spring Boot/HelloHuman/src/main/java/com/hellohuman/HelloHuman.java
@@ -0,0 +1,24 @@
+package com.hellohuman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+@SpringBootApplication
+@RestController
+
+public class HelloHuman {
+    @RequestMapping("/")
+    public String index(@RequestParam(value="name", required = false) String searchQuery,
+                        @RequestParam(value="last", required = false) String searchQuery1) {
+        if (searchQuery1 == null){
+            return "You searched for: " + searchQuery;
+        } else if (searchQuery1 != null){
+            return "You searched for: " + searchQuery +" " + searchQuery1;
+        }else {
+            return "Please set your name";
+        }
+    }
+}
